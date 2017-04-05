6338780eaabd0c0a73cc19ca172b6f940c23f359
bokken <negibokken@gmail.com>
2017-04-06 06:15:12 +0900

    Add test in ex03

--- ch01/ex03/ex03_test.go
+++ ch01/ex03/ex03_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
 
 func BenchmarkInefficientEcho(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -11,5 +15,53 @@ func BenchmarkInefficientEcho(b *testing.B) {
 func BenchmarkEfficientEcho(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		EfficientEcho([]string{"./ex03", "aa", "bb", "cc"})
+	}
+}
+
+func TestInefficientEcho(t *testing.T) {
+	programName := "ex03"
+	var tests = []struct {
+		args []string
+		want string
+	}{
+		{[]string{programName, "foo", "bar"}, fmt.Sprintf("%v foo bar\n", programName)},
+		{[]string{programName, "hoge"}, fmt.Sprintf("%v hoge\n", programName)},
+		{[]string{programName}, fmt.Sprintf("%v\n", programName)},
+	}
+
+	for _, test := range tests {
+		descr := fmt.Sprintf("InefficientEcho(%q)", test.args)
+		out = new(bytes.Buffer)
+		if err := InefficientEcho(test.args); err != nil {
+			t.Errorf("%s failed: %v", descr, err)
+		}
+		got := out.(*bytes.Buffer).String()
+		if got != test.want {
+			t.Errorf("%s = %q, want %q", descr, got, test.want)
+		}
+	}
+}
+
+func TestEfficientEcho(t *testing.T) {
+	programName := "ex03"
+	var tests = []struct {
+		args []string
+		want string
+	}{
+		{[]string{programName, "foo", "bar"}, fmt.Sprintf("%v foo bar\n", programName)},
+		{[]string{programName, "hoge"}, fmt.Sprintf("%v hoge\n", programName)},
+		{[]string{programName}, fmt.Sprintf("%v\n", programName)},
+	}
+
+	for _, test := range tests {
+		descr := fmt.Sprintf("EfficientEcho(%q)", test.args)
+		out = new(bytes.Buffer)
+		if err := InefficientEcho(test.args); err != nil {
+			t.Errorf("%s failed: %v", descr, err)
+		}
+		got := out.(*bytes.Buffer).String()
+		if got != test.want {
+			t.Errorf("%s = %q, want %q", descr, got, test.want)
+		}
 	}
 }
