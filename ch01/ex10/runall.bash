6c33dea2d5614af00f044311fa12fdef75522b5d
bokken <negibokken@gmail.com>
2017-04-09 11:54:38 +0900

    Add chapter1 ex10

---
+++ ch01/ex10/runall.bash
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+go build ex10.go
+./ex10 http://www.rakuten.co.jp http://www.rakuten.co.jp http://www.yahoo.co.jp http://www.yahoo.co.jp
+./ex10 http://www.rakuten.co.jp http://www.rakuten.co.jp http://www.yahoo.co.jp http://www.yahoo.co.jp
