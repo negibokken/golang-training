1ed3625dfcaa6d75f219580f2b234cee6d46c355
ogawa <ryoh.ogawa@nts.ricoh.co.jp>
2017-05-18 09:17:39 +0900

    Update file

--- ch04/ex11/runall.bash
+++ ch04/ex11/runall.bash
@@ -2,7 +2,7 @@
 
 go build
 # ./ex11 create git-api-test negibokken
-#./ex11 get git-api-test negibokken 1
+./ex11 get git-api-test negibokken 1
 # ./ex11 edit git-api-test negibokken 1
-./ex11 close git-api-test negibokken 1
+# ./ex11 close git-api-test negibokken 1
 
