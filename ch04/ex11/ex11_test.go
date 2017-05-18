2643e5fd1bd7f2df325806429523822a3be0b5b9
negibokken <negibokken@gmail.com>
2017-05-18 23:06:08 +0900

    Update test

--- ch04/ex11/ex11_test.go
+++ ch04/ex11/ex11_test.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+
+	"./github"
+)
+
+func TestNewClient(t *testing.T) {
+	var c github.Client
+	if reflect.TypeOf(github.NewClient("token")) != reflect.TypeOf(&c) {
+		t.Errorf("NewClient(token string) is not returned correct type. %v, %v",
+			reflect.TypeOf(github.NewClient("token")),
+			reflect.TypeOf(c))
+	}
+}
