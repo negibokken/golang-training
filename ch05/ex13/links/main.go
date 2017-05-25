db33c6836d7e75813df0784c4ad8f63c17729940
negibokken <negibokken@gmail.com>
2017-05-26 00:42:55 +0900

    Add chapter5 ex13

---
+++ ch05/ex13/links/main.go
@@ -0,0 +1,102 @@
+package links
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path/filepath"
+
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+// Extract links
+func Extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue
+				}
+				err = saveHTML(url, link.String())
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%v", err)
+					return
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return links, nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+func saveHTML(origin, link string) error {
+	resp, err := http.Get(link)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		return err
+	}
+	u, err := url.Parse(link)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		return err
+	}
+
+	dir := filepath.Join("./", u.Host, filepath.Clean(u.Path))
+	base := "[content]"
+
+	err = os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		return err
+	}
+
+	err = ioutil.WriteFile(filepath.Join(dir, base), bs, os.ModePerm)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		return err
+	}
+	return nil
+}
