8ea39512d8b0dfb3b8c8011634484113b333c117
negibokken <negibokken@gmail.com>
2017-06-07 20:35:43 +0900

    Update runall.bash

--- ch07/ex17/runall.bash
+++ ch07/ex17/runall.bash
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 go build
-cat test.xml| ./ex17 id xml-names
+# cat test.xml| ./ex17 id xml-names
 # cat test.xml| ./ex17 id sec-guessing-with-ext-info
-# cat test.xml| ./ex17 class div1
+cat test.xml| ./ex17 class prefix
