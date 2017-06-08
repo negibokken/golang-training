761085b5a44c2ca717f4d85c3faa6853d4fbb4db
negibokken <negibokken@gmail.com>
2017-06-08 20:33:22 +0900

    Update test

--- ch07/ex17/main_test.go
+++ ch07/ex17/main_test.go
@@ -1,1 +1,92 @@
 package main
+
+import "testing"
+
+func Test_containsAll(t *testing.T) {
+	type args struct {
+		x []string
+		y []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"test containsAll",
+			args{
+				[]string{""},
+				[]string{""},
+			},
+			true,
+		},
+		{
+			"test containsAll",
+			args{
+				[]string{"a", "b"},
+				[]string{},
+			},
+			true,
+		},
+		{
+			"test containsAll",
+			args{
+				[]string{"a", "b"},
+				[]string{"c"},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := containsAll(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("containsAll() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_hasTarget(t *testing.T) {
+	type args struct {
+		attrStack []attr
+		id        string
+		value     string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"test has Target",
+			args{
+				[]attr{
+					attr{"id", "id-test"},
+					attr{"class", "class-test"},
+				},
+				"id",
+				"id-test",
+			},
+			true,
+		},
+		{
+			"test has Target",
+			args{
+				[]attr{
+					attr{"id", "id-test"},
+					attr{"class", "class-test"},
+				},
+				"class",
+				"id-test",
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hasTarget(tt.args.attrStack, tt.args.id, tt.args.value); got != tt.want {
+				t.Errorf("hasTarget() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- ch07/ex18/main_test.go
+++ ch07/ex18/main_test.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"encoding/xml"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func Test_makeTree(t *testing.T) {
+	dec := xml.NewDecoder(strings.NewReader(`
+		<html>
+			<head></head>
+			<body><p>hello</p></body>
+		</html>
+	`))
+	tests := []struct {
+		name    string
+		args    *xml.Decoder
+		want    Node
+		wantErr bool
+	}{
+		{
+			"test makeTree",
+			dec,
+			[]Node{
+				Element{
+					xml.Name{"", "html"},
+					[]xml.Attr{},
+					[]Node{},
+				},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := makeTree(tt.args)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("makeTree() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !equal(got, tt.want) {
+				t.Errorf("makeTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func equal(got, want Node) bool {
+	gotE := got.(*Element)
+	wantE := got.(*Element)
+	if !reflect.DeepEqual(gotE.Attr, wantE.Attr) {
+		return false
+	}
+	if !reflect.DeepEqual(gotE.Type, wantE.Type) {
+		return false
+	}
+	return true
+}
