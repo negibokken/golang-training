3389d084a34fae0286bb40608d6dd71553f7889e
negibokken <negibokken@gmail.com>
2017-06-13 22:44:26 +0900

    Update files

--- ch08/ex01/clockwall/clockwall.go
+++ ch08/ex01/clockwall/clockwall.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
-	"log"
 	"net"
 	"os"
 	"strings"
@@ -19,31 +18,42 @@ func main() {
 }
 
 func clockWall(args ...string) {
-	for _, arg := range args[1:] {
+	done := make([]chan string, len(args[1:]))
+	for i, arg := range args[1:] {
 		pair := strings.Split(arg, "=")
-		if len(pair) == 1 {
+		if len(pair) != 2 {
 			fmt.Fprintf(os.Stderr, "./cloclwall Tokyo=localhost:8000 London=localhost:8001")
 			os.Exit(1)
 		}
-		conn, err := net.Dial("tcp", pair[1])
+		host := pair[1]
+		done[i] = make(chan string)
+		go dial(host, done[i])
+	}
+	for {
+		for i, arg := range args[1:] {
+			pair := strings.Split(arg, "=")
+			name := pair[0]
+			fmt.Printf("%s %s", name, <-done[i])
+		}
+	}
+}
+
+func dial(host string, done chan<- string) {
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	read := bufio.NewReader(conn)
+	for {
+		str, err := read.ReadString('\n')
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v", err)
 			os.Exit(1)
 		}
-		defer conn.Close()
-		go handleConn(conn, pair[0])
-	}
-}
-
-func handleConn(c net.Conn, location string) {
-	for {
-		fmt.Fprintf(os.Stdout, "Location: %v", location)
-		mustCopy(os.Stdout, c)
-	}
-}
-
-func mustCopy(dst io.Writer, src io.Reader) {
-	if _, err := io.Copy(dst, src); err != nil {
-		log.Fatal(err)
+		done <- str
 	}
 }
---
+++ ch08/ex01/clockwall/runall.bash
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+go build
+./clockwall US=localhost:8001 London=localhost:8002
