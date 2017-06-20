ae662fbeaeeb0c1e1f0cf57121041d813e07503b
negibokken <negibokken@gmail.com>
2017-06-20 23:21:48 +0900

    Update test program

--- ch08/ex02/main.go
+++ ch08/ex02/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go func() {
+		for x := 0; x < 100; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}()
+
+	go func() {
+		for x := range naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	for x := range squares {
+		fmt.Println(x)
+	}
 }
