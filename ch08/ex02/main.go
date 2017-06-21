c29f31a3cb91318026d2f710164d74cab7942b73
negibokken <negibokken@gmail.com>
2017-06-22 06:21:25 +0900

    Update file

--- ch08/ex02/main.go
+++ ch08/ex02/main.go
@@ -1,29 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
 
 func main() {
-	go spinner(100 * time.Millisecond)
-	const n = 45
-	fibN := fib(n)
-	fmt.Printf("\r Fibonacci(%d) = %d\n", n, fibN)
-}
+	naturals := make(chan int)
+	squares := make(chan int)
 
-func spinner(delay time.Duration) {
-	for {
-		for _, r := range `-||/` {
-			fmt.Printf("\r%c", r)
-			time.Sleep(delay)
-		}
-	}
-}
-
-func fib(x int) int {
-	if x < 2 {
-		return x
-	}
-	return fib(x-1) + fib(x-2)
+	go counter(naturals)
+	go squarer(squares, naturals)
+	printer(squares)
 }
