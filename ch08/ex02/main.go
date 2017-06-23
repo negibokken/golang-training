238ed042323340749193edd3d6ee611d0314c4a3
negibokken <negibokken@gmail.com>
2017-06-23 23:21:07 +0900

    Update

--- ch08/ex02/main.go
+++ ch08/ex02/main.go
@@ -4,29 +4,31 @@ import "fmt"
 
 func counter(out chan<- int) {
 	for x := 0; x < 100; x++ {
+		fmt.Println(x)
 		out <- x
 	}
 	close(out)
 }
 
-func squarer(out chan<- int, in <-chan int) {
-	for v := range in {
-		out <- v * v
-	}
-	close(out)
-}
-
-func printer(in <-chan int) {
-	for v := range in {
-		fmt.Println(v)
-	}
-}
+// func squarer(out chan<- int, in <-chan int) {
+// 	for v := range in {
+// 		out <- v * v
+// 	}
+// 	close(out)
+// }
+//
+// func printer(in <-chan int) {
+// 	for v := range in {
+// 		fmt.Println(v)
+// 	}
+// }
 
 func main() {
 	naturals := make(chan int)
-	squares := make(chan int)
+	// squares := make(chan int)
 
 	go counter(naturals)
-	go squarer(squares, naturals)
-	printer(squares)
+	fmt.Println(<-naturals)
+	// go squarer(squares, naturals)
+	// printer(squares)
 }
--- ch08/ex02/runall.bash
+++ ch08/ex02/runall.bash
@@ -1,3 +1,4 @@
 #! /bin/sh
 
 go build
+./ex02
