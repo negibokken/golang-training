b8f9a8e28c108e83436bbe6ba19470d3ada28fd6
negibokken <negibokken@gmail.com>
2017-07-11 02:37:10 +0900

    Update chpater9 ex04

--- ch09/ex04/ex04_test.go
+++ ch09/ex04/ex04_test.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 )
 
-func BenchmarkPipeLine10(b *testing.B)    { benchmarkMyPipeLine(b, 10) }
-func BenchmarkPipeLine100(b *testing.B)   { benchmarkMyPipeLine(b, 100) }
-func BenchmarkPipeLine1000(b *testing.B)  { benchmarkMyPipeLine(b, 1000) }
-func BenchmarkPipeLine10000(b *testing.B) { benchmarkMyPipeLine(b, 10000) }
+func BenchmarkPipeLine10(b *testing.B)      { benchmarkMyPipeLine(b, 10) }
+func BenchmarkPipeLine100(b *testing.B)     { benchmarkMyPipeLine(b, 100) }
+func BenchmarkPipeLine1000(b *testing.B)    { benchmarkMyPipeLine(b, 1000) }
+func BenchmarkPipeLine10000(b *testing.B)   { benchmarkMyPipeLine(b, 10000) }
+func BenchmarkPipeLine100000(b *testing.B)  { benchmarkMyPipeLine(b, 100000) }
+func BenchmarkPipeLine1000000(b *testing.B) { benchmarkMyPipeLine(b, 1000000) }
 
 func benchmarkMyPipeLine(b *testing.B, n int) {
+	fmt.Printf("\n---- Number of Goroutine: %d ----", n)
+	var startMemory runtime.MemStats
+	runtime.ReadMemStats(&startMemory)
 	in, out := MyPipeLine(n)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		go func() { in <- struct{}{} }()
 		<-out
 	}
+
+	var endMemory runtime.MemStats
+	runtime.ReadMemStats(&endMemory)
+
+	fmt.Printf("\nmemory all: %f MB\n",
+		float64(endMemory.Alloc-startMemory.Alloc)/float64(1024*1024))
 }
