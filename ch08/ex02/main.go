ddecfa20e5e0879238913e0e8073f548338ef122
negibokken <negibokken@gmail.com>
2017-07-12 20:25:35 +0900

    Update

--- ch08/ex02/main.go
+++ ch08/ex02/main.go
@@ -1,34 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
-func counter(out chan<- int) {
-	for x := 0; x < 100; x++ {
-		fmt.Println(x)
-		out <- x
-	}
-	close(out)
+var protocolINterpreterClose chan struct{}
+var dataTransProcessClose chan struct{}
+
+func commandList() {
+	// RETR 取得
+	// STOR 保存
+	// STOU 一位保存
+	// APPE	追加（生成）
+	// ALLO 割当
+	// REST 再開
+	// RNFR 名称変更元
+	// RNTO 名称変更先
+	// ABOR 中断
+	// DELE 削除
+	// RMD  ディレクトリ削除
+	// MKD  ディレクトリ作成
+	// PWD  作業ディレクトリ表示
+	// LIST 一覧
+	// NLST 名前一覧
+	// SITE サイト固有パラメータ
+	// SYST システム
+	// STAT ステータス
+	// HELP ヘルプ
+	// NOOP NOOP
 }
 
-// func squarer(out chan<- int, in <-chan int) {
-// 	for v := range in {
-// 		out <- v * v
-// 	}
-// 	close(out)
-// }
-//
-// func printer(in <-chan int) {
-// 	for v := range in {
-// 		fmt.Println(v)
-// 	}
-// }
+func protocolInterpreter() {
+
+}
+
+func dataTransferProcess() {
+
+}
+
+func handleConn(conn net.Conn) {
+	fmt.Println("A client connected")
+	fmt.Fprintf(conn, "Connected to FTP server\n")
+
+	scanner := bufio.NewScanner(conn)
+	fmt.Print("CMD>")
+	for scanner.Scan() {
+		cmd := scanner.Text()
+		fmt.Printf("%v\n", cmd)
+		fmt.Print("CMD>")
+	}
+}
 
 func main() {
-	naturals := make(chan int)
-	// squares := make(chan int)
-
-	go counter(naturals)
-	fmt.Println(<-naturals)
-	// go squarer(squares, naturals)
-	// printer(squares)
+	listener, err := net.Listen("tcp", "localhost:21")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		return
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("%v\n", err)
+			conn.Close()
+			return
+		}
+		go handleConn(conn)
+	}
 }
