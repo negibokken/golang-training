ff06dafc98a6fb6e99d496f7790c65285005e595
negibokken <negibokken@gmail.com>
2017-07-27 23:30:43 +0900

    Update files

---
+++ ch10/ex02/archive/zip/zip.go
@@ -0,0 +1,65 @@
+package zip
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+
+	archives "github.com/negibokken/gopl/ch10/ex02"
+)
+
+type reader struct {
+	zipReader *zip.Reader
+	filesLeft []*zip.File
+	r         io.ReadCloser
+	toWrite   string
+}
+
+func (r *reader) Read(b []byte) (int, error) {
+	if r.r == nil && len(r.filesLeft) == 0 { // EOF
+		return 0, io.EOF
+	}
+	if r.r == nil {
+		f := r.filesLeft[0]
+		r.filesLeft = r.filesLeft[1:]
+		var err error
+		r.r, err = f.Open()
+		if err != nil {
+			return 0, fmt.Errorf("read zip: %s", err)
+		}
+	}
+	written := 0
+	if len(r.toWrite) > 0 {
+		n := copy(b, r.toWrite)
+		b = b[n:]
+		r.toWrite = r.toWrite[n:]
+		written += n
+	}
+	n, err := r.r.Read(b)
+	written += n
+	if err != nil {
+		r.r.Close()
+		r.r = nil
+		if err == io.EOF {
+			return written, nil
+		}
+	}
+	return written, nil
+}
+
+func NewReader(f *os.File) (io.Reader, error) {
+	stat, err := f.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("new zip reader: %s", err)
+	}
+	r, err := zip.NewReader(f, stat.Size())
+	if err != nil {
+		return nil, fmt.Errorf("new zip reader: %s", err)
+	}
+	return &reader{r, r.File, nil, ""}, nil
+}
+
+func init() {
+	archives.RegisterFormat("zip", "PK", 0, NewReader)
+}
--- ch10/ex02/ex02.go
+++ ch10/ex02/ex02.go
@@ -1,18 +1,45 @@
-package main
+package archives
 
 import (
-	_ "archive/tar"
-	_ "archive/zip"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
 )
 
-func main() {
-
+type format struct {
+	nam, magic  string
+	magicOffset int
+	reader      NewReader
 }
 
-var readZip = func() {
+type NewReader func(*os.File) (io.Reader, error)
 
+var formats []format
+
+func Registerformat(name, magic string, magicOffset int, f NewReader) {
+	formats = append(formats, format{name, magic, magicOffset, f})
 }
 
-var readTar = func() {
-
+func Open(file *os.File) (io.Reader, error) {
+	var found *format
+	r := bufio.NewReader(file)
+	for _, f := range formats {
+		p, err := r.Peek(f.magicOffset + len(f.magic))
+		if err != nil {
+			continue
+		}
+		if string(p[f.magicOffset:]) == f.magic {
+			found = &f
+			break
+		}
+	}
+	if found == nil {
+		return nil, fmt.Errorf("open archive: can't determine format")
+	}
+	_, err := file.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, fmt.Errorf("open archive: %s", err)
+	}
+	return found.reader(file)
 }
