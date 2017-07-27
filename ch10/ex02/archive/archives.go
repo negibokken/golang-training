c7460d1cf7b254198934624891cbcdeef374a7a7
negibokken <negibokken@gmail.com>
2017-07-28 00:21:55 +0900

    Update file

--- ch10/ex02/archive/archives.go
+++ ch10/ex02/archive/archives.go
@@ -17,7 +17,7 @@ type NewReader func(*os.File) (io.Reader, error)
 
 var formats []format
 
-func Registerformat(name, magic string, magicOffset int, f NewReader) {
+func RegisterFormat(name, magic string, magicOffset int, f NewReader) {
 	formats = append(formats, format{name, magic, magicOffset, f})
 }
 
