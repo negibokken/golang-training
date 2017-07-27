4594e0fec1caa9a50d3f219794b50a4ae0e86c3e
negibokken <negibokken@gmail.com>
2017-07-28 00:21:02 +0900

    Update files

--- ch10/ex02/ex02.go
+++ ch10/ex02/ex02.go
@@ -1,45 +1,9 @@
-package archives
+package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
+	_ "github.com/negibokken/golang-training/ch10/ex02/archive/tar"
 )
 
-type format struct {
-	nam, magic  string
-	magicOffset int
-	reader      NewReader
-}
+func main() {
 
-type NewReader func(*os.File) (io.Reader, error)
-
-var formats []format
-
-func Registerformat(name, magic string, magicOffset int, f NewReader) {
-	formats = append(formats, format{name, magic, magicOffset, f})
-}
-
-func Open(file *os.File) (io.Reader, error) {
-	var found *format
-	r := bufio.NewReader(file)
-	for _, f := range formats {
-		p, err := r.Peek(f.magicOffset + len(f.magic))
-		if err != nil {
-			continue
-		}
-		if string(p[f.magicOffset:]) == f.magic {
-			found = &f
-			break
-		}
-	}
-	if found == nil {
-		return nil, fmt.Errorf("open archive: can't determine format")
-	}
-	_, err := file.Seek(0, os.SEEK_SET)
-	if err != nil {
-		return nil, fmt.Errorf("open archive: %s", err)
-	}
-	return found.reader(file)
 }
