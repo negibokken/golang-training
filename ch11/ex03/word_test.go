cb24d8d8c48a987588de824edba172fb2398de7f
negibokken <negibokken@gmail.com>
2017-08-06 16:42:19 +0900

    Add chapter11 ex03

---
+++ ch11/ex03/word_test.go
@@ -0,0 +1,48 @@
+package word
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func randomNonPalindrome(rng *rand.Rand) string {
+	str := randomPalindrome(rng)
+	// aXb != bXa
+	return "a" + str + "b"
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
+
+func TestRandomNonPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		np := randomNonPalindrome(rng)
+		if IsPalindrome(np) {
+			t.Errorf("IsPalindrome(%q) = true", np)
+		}
+	}
+}
