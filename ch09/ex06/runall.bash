b0e2c40cd494fef226d53757c9cc98c17439264b
ogawa <ryoh.ogawa@nts.ricoh.co.jp>
2017-08-10 11:06:06 +0900

    Update ch09 ex06

--- ch09/ex06/runall.bash
+++ ch09/ex06/runall.bash
@@ -1,4 +1,7 @@
 #! /bin/sh
 
 go build
-./ex06 > sample.png
+GOMAXPROCS=1 ./ex06 > sample1.png
+GOMAXPROCS=2 ./ex06 > sample2.png
+GOMAXPROCS=3 ./ex06 > sample3.png
+GOMAXPROCS=4 ./ex06 > sample4.png
